22dde399ad5dd6d39b61b22cf9bb2400ab07cc4f
Thomas Cools <tacools10@gmail.com>
2025-12-31 02:35:58 +0000

    Extract Footer into separate component

--- src/App.tsx
+++ src/App.tsx
@@ -53,8 +53,7 @@ import weddingLogo2x from './assets/monogram_websiteT&C-2x.webp'
 import weddingLogoFull from './assets/T&C-Monogram.webp'
 import airbnbLogo from './assets/airbnb-tile.svg'
 import bookingLogo from './assets/booking-tile.svg'
-import footerDetail from './assets/footer_detail.svg'
-import signatureSvg from './assets/carolina_and_thomas_signature.svg'
+import Footer from './components/Footer'
 
 // Optimized WebP images for hero collage
 import envelopeSmall from './assets/envelope-400.webp'
@@ -510,62 +509,7 @@ function AppContent() {
         </Box>
       </Box>
 
-      {/* Footer - Elegant Dark Design */}
-      <Box as="footer" position="relative" overflow="hidden">
-        {/* Decorative scalloped border - positioned to overlap footer */}
-        <ChakraImage
-          src={footerDetail}
-          alt=""
-          w="100%"
-          h="auto"
-          display="block"
-          minW="1440px"
-          objectFit="cover"
-          objectPosition="center"
-          transform="translateX(-50%)"
-          position="relative"
-          left="50%"
-          mb="-31px"
-        />
-        
-        {/* Footer content */}
-        <Box 
-          bg="#300F0C" 
-          py={16} 
-          textAlign="center"
-        >
-          <Container maxW="container.lg">
-            <ScrollReveal>
-              <VStack spacing={6}>
-                <Text 
-                  color="white" 
-                  fontSize="md" 
-                  fontFamily="body"
-                  letterSpacing="0.05em"
-                >
-                  {t('footer.contactUs')}
-                </Text>
-                <Link
-                  href="mailto:carolinaandthomaswedding@gmail.com"
-                  color="white"
-                  fontSize="md"
-                  fontFamily="body"
-                  letterSpacing="0.02em"
-                  _hover={{ opacity: 0.8, textDecoration: 'underline' }}
-                >
-                  carolinaandthomaswedding@gmail.com
-                </Link>
-                <ChakraImage
-                  src={signatureSvg}
-                  alt="Carolina & Thomas"
-                  maxW={{ base: '200px', md: '280px' }}
-                  mt={6}
-                />
-              </VStack>
-            </ScrollReveal>
-          </Container>
-        </Box>
-      </Box>
+      <Footer />
     </Box>
     </>
   )
---
+++ src/components/Footer.tsx
@@ -0,0 +1,77 @@
+import { useTranslation } from 'react-i18next'
+import {
+  Box,
+  Container,
+  Image as ChakraImage,
+  Link,
+  Text,
+  VStack,
+} from '@chakra-ui/react'
+import { ScrollReveal } from './animations'
+import footerDetail from '../assets/footer_detail.svg'
+import signatureSvg from '../assets/carolina_and_thomas_signature.svg'
+
+/**
+ * Footer component with scalloped border and contact information
+ */
+export default function Footer() {
+  const { t } = useTranslation()
+
+  return (
+    <Box as="footer" position="relative" overflow="hidden">
+      {/* Decorative scalloped border - positioned to overlap footer */}
+      <ChakraImage
+        src={footerDetail}
+        alt=""
+        w="100%"
+        h="auto"
+        display="block"
+        minW="1440px"
+        objectFit="cover"
+        objectPosition="center"
+        transform="translateX(-50%)"
+        position="relative"
+        left="50%"
+        mb="-31px"
+      />
+      
+      {/* Footer content */}
+      <Box 
+        bg="#300F0C" 
+        py={16} 
+        textAlign="center"
+      >
+        <Container maxW="container.lg">
+          <ScrollReveal>
+            <VStack spacing={6}>
+              <Text 
+                color="white" 
+                fontSize="md" 
+                fontFamily="body"
+                letterSpacing="0.05em"
+              >
+                {t('footer.contactUs')}
+              </Text>
+              <Link
+                href="mailto:carolinaandthomaswedding@gmail.com"
+                color="white"
+                fontSize="md"
+                fontFamily="body"
+                letterSpacing="0.02em"
+                _hover={{ opacity: 0.8, textDecoration: 'underline' }}
+              >
+                carolinaandthomaswedding@gmail.com
+              </Link>
+              <ChakraImage
+                src={signatureSvg}
+                alt="Carolina & Thomas"
+                maxW={{ base: '200px', md: '280px' }}
+                mt={6}
+              />
+            </VStack>
+          </ScrollReveal>
+        </Container>
+      </Box>
+    </Box>
+  )
+}
