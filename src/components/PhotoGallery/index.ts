03a87943a1d99ef828cebc47d679a382e4b85f65
Thomas Cools <tacools10@gmail.com>
2026-01-03 17:04:25 +0000

    Delete unused PhotoGallery component and tests

--- src/components/PhotoGallery/index.ts
+++
@@ -1,6 +0,0 @@
-// PhotoGallery module barrel export
-export { PhotoGallery } from './PhotoGallery'
-export { GalleryGrid } from './GalleryGrid'
-export { GalleryLightbox } from './GalleryLightbox'
-export { useGalleryNavigation } from './useGalleryNavigation'
-export type { GalleryImage, PhotoGalleryProps } from './types'
